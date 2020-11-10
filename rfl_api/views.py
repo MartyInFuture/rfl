f233b716d05cc4993d988c644d7395ae9c1c10b6
MartyInFuture <martyinfuture@gmail.com>
2020-11-10 12:18:37 +0200

    coomit viewsets learn

--- rfl_api/views.py
+++ rfl_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+
 from . import serializers
 
 
@@ -43,3 +45,43 @@ class HelloApiView(APIView):
         return Response({'method': 'DELETE'})
          
 
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+
+    # Test API ViewSet
+    def list(self, request):
+        # Return a hello message
+        a_viewset = [
+            'Uses actions(list, create, reuriece, update, partial_update)', 
+            'Automatically maps to URLs using Routers', 
+            'Provides more functionality with less code', 
+        ]
+
+        return Response({'message': 'Hello', 'a_viewset': a_viewset}, status=status.HTTP_200_OK)
+
+    def create(self, request): 
+        # Create a new hello message
+        serializer = self.serializer_class(data=request.data) 
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else: 
+            return Response(
+                serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    def retrieve(self, request, pk=None): 
+        # Handle getting an object bu its Id
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method': 'PUT'})
+    
+    def partial_update(self, request, pk=None): 
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None): 
+        return Response({'http_method': 'DELETE'})
