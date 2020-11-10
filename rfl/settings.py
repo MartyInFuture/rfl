204cf0ea2d6f9aea73490e1329f8e9272170c65e
MartyInFuture <martyinfuture@gmail.com>
2020-11-10 15:44:25 +0200

    Contigured for deplayment

--- rfl/settings.py
+++ rfl/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ycn*(q4@fb3-l_-h)zt48b1=#tft=(#&^8!tbqw&q7lf8s0@_r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', 1))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'rfl_api.UserProfile'
+
+STATIC_ROOT = 'static/'
